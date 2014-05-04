cd4d42cebbd51323d20eacbc4e616a748180e330
Martin Flucka <martin.flucka@gmail.com>
2014-05-04 19:23:40 +0200

    proper indentation

--- test/resources/charge_tests.dart
+++ test/resources/charge_tests.dart
@@ -7,25 +7,25 @@ import 'package:unittest/unittest.dart';
 import '../../lib/stripe.dart';
 
 int amount;
-  int created;
-  String currency;
-  String id;
-  bool livemode;
-  bool paid;
-  bool refunded;
-  bool disputed;
-  bool captured;
-  String description;
-  String failureMessage;
-  String failureCode;
-  int amountRefunded;
-  String customer;
-  String invoice;
-  List<Refund> refunds;
-  Card card;
-  Dispute dispute;
-  String balanceTransaction;
-  Map<String, String> metadata;
+int created;
+String currency;
+String id;
+bool livemode;
+bool paid;
+bool refunded;
+bool disputed;
+bool captured;
+String description;
+String failureMessage;
+String failureCode;
+int amountRefunded;
+String customer;
+String invoice;
+List<Refund> refunds;
+Card card;
+Dispute dispute;
+String balanceTransaction;
+Map<String, String> metadata;
 
 var exampleObject = """
                     {
