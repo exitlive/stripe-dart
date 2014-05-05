55ee934131f27914406ec0c2ada53d7bb02210fd
Martin Flucka <martin.flucka@gmail.com>
2014-05-05 20:03:12 +0200

    implemented creation and update for customer

--- lib/src/resources/customer.dart
+++ lib/src/resources/customer.dart
@@ -76,16 +76,27 @@ class Customer extends ApiResource {
 
 
 /**
- * STUB.
- * TODO: Complete this class.
+ * Used to create a new [Customer]
  */
 class CustomerCreation extends ResourceRequest {
 
-  set description (String desc) => _setMap("description", desc);
+  set accountBalance (int accountBalance) => _setMap("account_balance", accountBalance);
 
   set card (CardCreation card) => _setMap("card", card._getMap());
 
-  set cardId (String cardId) =>  _setMap("card", cardId);
+  set coupon (String coupon) => _setMap("coupon", coupon);
+
+  set description (String description) => _setMap("description", description);
+
+  set email (String email) => _setMap("email", email);
+
+  set metadata (Map metadata) => _setMap("metadata", metadata);
+
+  set plan (String plan) => _setMap("plan", plan);
+
+  set quantity (int quantity) => _setMap("quantity", quantity);
+
+  set trialEnd (int trialEnd) => _setMap("trial_end", trialEnd);
 
   Future<Customer> create() {
     return StripeService.create(Customer._path, _getMap())
@@ -96,12 +107,23 @@ class CustomerCreation extends ResourceRequest {
 
 
 /**
- * STUB.
- * TODO: Complete this class.
+ * Used to update an existing [Customer]
  */
 class CustomerUpdate extends ResourceRequest {
 
-  set description (String desc) => _setMap("description", desc);
+  set accountBalance (int accountBalance) => _setMap("account_balance", accountBalance);
+
+  set card (CardCreation card) => _setMap("card", card._getMap());
+
+  set coupon (String coupon) => _setMap("coupon", coupon);
+
+  set defaultCard (String defaultCard) => _setMap("default_card", defaultCard);
+
+  set description (String description) => _setMap("description", description);
+
+  set email (String email) => _setMap("email", email);
+
+  set metadata (Map metadata) => _setMap("metadata", metadata);
 
   Future<Customer> update() {
     return StripeService.create(Customer._path, _getMap())
