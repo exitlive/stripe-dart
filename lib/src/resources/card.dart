b048ed5f68ffc3710b3d1e83af5100796babbdb4
Martin Flucka <martin.flucka@gmail.com>
2014-05-05 13:45:44 +0200

    implemented card
    added basic test as well

--- lib/src/resources/card.dart
+++ lib/src/resources/card.dart
@@ -2,29 +2,49 @@ part of stripe;
 
 class Card extends Resource {
 
-  String objectName = "card";
-  int expMonth;
-  int expYear;
-  String last4;
-  String country;
-  String type;
-  String name;
-  String id;
-  String customer;
-  String addressLine1;
-  String addressLine2;
-  String addressZip;
-  String addressCity;
-  String addressState;
-  String addressCountry;
-  String addressZipCheck;
-  String addressLine1Check;
-  String cvcCheck;
-  String fingerprint;
+  final String objectName = "card";
+
+  static String _path = "cards";
+
 
   Card.fromMap(Map map) : super.fromMap(map) {
 
   }
 
+  int get expMonth => _dataMap["exp_month"];
+
+  int get expYear => _dataMap["exp_year"];
+
+  String get last4 => _dataMap["last4"];
+
+  String get country => _dataMap["country"];
+
+  String get type => _dataMap["type"];
+
+  String get name => _dataMap["name"];
+
+  String get id => _dataMap["id"];
+
+  String get customer => _dataMap["customer"];
+
+  String get addressLine1 => _dataMap["address_line1"];
+
+  String get addressLine2 => _dataMap["address_line2"];
+
+  String get addressZip => _dataMap["address_zip"];
+
+  String get addressCity => _dataMap["address_city"];
+
+  String get addressState => _dataMap["address_state"];
+
+  String get addressCountry => _dataMap["address_country"];
+
+  String get addressZipCheck => _dataMap["address_zip_check"];
+
+  String get addressLine1Check => _dataMap["address_line1_check"];
+
+  String get cvcCheck => _dataMap["cvc_check"];
+
+  String get fingerprint => _dataMap["fingerprint"];
 
 }
---
+++ test/resources/card_tests.dart
@@ -0,0 +1,63 @@
+library card_tests;
+
+import "dart:convert";
+
+import 'package:unittest/unittest.dart';
+
+import '../../lib/stripe.dart';
+
+var exampleObject = """
+                    {
+                      "id": "card_103yOK2eZvKYlo2CNWdHfG5K",
+                      "object": "card",
+                      "last4": "4242",
+                      "type": "Visa",
+                      "exp_month": 1,
+                      "exp_year": 2050,
+                      "fingerprint": "Xt5EWLLDS7FJjR1c",
+                      "customer": null,
+                      "country": "US",
+                      "name": null,
+                      "address_line1": null,
+                      "address_line2": null,
+                      "address_city": null,
+                      "address_state": null,
+                      "address_zip": null,
+                      "address_country": null,
+                      "cvc_check": "pass",
+                      "address_line1_check": null,
+                      "address_zip_check": null
+                    }""";
+
+
+main() {
+
+  group('Card', () {
+
+
+    test("fromMap() properly popullates all values", () {
+      var map = JSON.decode(exampleObject);
+
+      var card = new Card.fromMap(map);
+
+      expect(card.id, equals(map["id"]));
+      expect(card.last4, equals(map["last4"]));
+      expect(card.type, equals(map["type"]));
+      expect(card.expMonth, equals(map["exp_month"]));
+      expect(card.expYear, equals(map["exp_year"]));
+      expect(card.fingerprint, equals(map["fingerprint"]));
+      expect(card.customer, equals(map["customer"]));
+      expect(card.addressLine1, equals(map["address_line1"]));
+      expect(card.addressLine2, equals(map["address_line2"]));
+      expect(card.addressCity, equals(map["address_city"]));
+      expect(card.addressState, equals(map["address_state"]));
+      expect(card.addressZip, equals(map["address_zip"]));
+      expect(card.addressCountry, equals(map["address_country"]));
+      expect(card.cvcCheck, equals(map["cvc_check"]));
+      expect(card.addressLine1Check, equals(map["address_line1_check"]));
+      expect(card.addressZipCheck, equals(map["address_zip_check"]));
+
+    });
+  });
+
+}
