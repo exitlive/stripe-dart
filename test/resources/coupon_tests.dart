99d10fe99dd65dd931672e25ab0ed98af6c41f1f
Martin Flucka <martin.flucka@gmail.com>
2014-05-11 22:13:39 +0200

    Moved coupon tests into group

--- test/resources/coupon_tests.dart
+++ test/resources/coupon_tests.dart
@@ -62,68 +62,67 @@ main(List<String> args) {
       expect(coupon.metadata, equals(map["metadata"]));
 
     });
-  });
 
+    test("CouponCreation minimal", () {
 
-  test("CouponCreation minimal", () {
+      // Coupon fields
+      String testDuration = "forever";
+      int testPercentOff = 5;
 
-    // Coupon fields
-    String testDuration = "forever";
-    int testPercentOff = 5;
+      Future future = (
+          new CouponCreation()
+              ..duration = testDuration
+              ..percentOff = testPercentOff
+          ).create();
 
-    Future future = (
-        new CouponCreation()
-            ..duration = testDuration
-            ..percentOff = testPercentOff
-        ).create();
+      future.then((Coupon coupon) {
+        expect(coupon.id, new isInstanceOf<String>());
+        expect(coupon.duration, testDuration);
+        expect(coupon.percentOff, testPercentOff);
+      });
 
-    future.then((Coupon coupon) {
-      expect(coupon.id, new isInstanceOf<String>());
-      expect(coupon.duration, testDuration);
-      expect(coupon.percentOff, testPercentOff);
+      expect(future, completes);
+
     });
 
-    expect(future, completes);
 
-  });
+    test("CouponCreation full", () {
 
+      // Coupon fields
+      String testId = "test id";
+      String testDuration = "repeating";
+      int testAmountOff = 10;
+      String testCurrency = "usd";
+      int testDurationInMoths = 12;
+      int testMaxRedemptions = 3;
+      Map testMetadata = {"foo": "bar"};
+      int testRedeemBy = 1451520000;
 
-  test("CouponCreation full", () {
+      Future future = (
+          new CouponCreation()
+              ..id = testId
+              ..duration = testDuration
+              ..amountOff = testAmountOff
+              ..currency = testCurrency
+              ..durationInMonths = testDurationInMoths
+              ..maxRedemptions = testMaxRedemptions
+              ..metadata = testMetadata
+              ..redeemBy = testRedeemBy
+          ).create();
 
-    // Coupon fields
-    String testId = "test id";
-    String testDuration = "repeating";
-    int testAmountOff = 10;
-    String testCurrency = "usd";
-    int testDurationInMoths = 12;
-    int testMaxRedemptions = 3;
-    Map testMetadata = {"foo": "bar"};
-    int testRedeemBy = 1451520000;
+      future.then((Coupon coupon) {
+        expect(coupon.id, testId);
+        expect(coupon.duration, testDuration);
+        expect(coupon.amountOff, testAmountOff);
+        expect(coupon.currency, testCurrency);
+        expect(coupon.durationInMonths, testDurationInMoths);
+        expect(coupon.maxRedemptions, testMaxRedemptions);
+        expect(coupon.metadata, testMetadata);
+        expect(coupon.redeemBy, testRedeemBy);
+      });
 
-    Future future = (
-        new CouponCreation()
-            ..id = testId
-            ..duration = testDuration
-            ..amountOff = testAmountOff
-            ..currency = testCurrency
-            ..durationInMonths = testDurationInMoths
-            ..maxRedemptions = testMaxRedemptions
-            ..metadata = testMetadata
-            ..redeemBy = testRedeemBy
-        ).create();
+      expect(future, completes);
 
-    future.then((Coupon coupon) {
-      expect(coupon.id, testId);
-      expect(coupon.duration, testDuration);
-      expect(coupon.amountOff, testAmountOff);
-      expect(coupon.currency, testCurrency);
-      expect(coupon.durationInMonths, testDurationInMoths);
-      expect(coupon.maxRedemptions, testMaxRedemptions);
-      expect(coupon.metadata, testMetadata);
-      expect(coupon.redeemBy, testRedeemBy);
     });
-
-    expect(future, completes);
-
   });
 }
