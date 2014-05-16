2aafa6e365b78a7d446a34997e60dd7422157d6f
Martin Flucka <martin.flucka@gmail.com>
2014-05-16 16:21:58 +0200

    Added list for charge

--- lib/src/resources/charge.dart
+++ lib/src/resources/charge.dart
@@ -187,6 +187,54 @@ class Charge extends Resource {
     return StripeService.post([Charge._path, id, "capture"], data: data)
         .then((Map json) => new Charge.fromMap(json));
   }
+
+  /**
+   * Returns a [ChargeCollection] of your charges.
+   * Returns a list of charges you've previously created. The charges are
+   * returned in sorted order, with the most recent charges appearing first.
+   *
+   * [created]: A filter on the list based on the object created field.
+   * The value can be a string with an exact UTC timestamp, or it can be a
+   * dictionary with the following options:
+   *   **gt** optional
+   *   Return values where the created field is after this timestamp.
+   *   **gte** optional
+   *   Return values where the created field is after or equal to this timestamp.
+   *   **lt** optional
+   *   Return values where the created field is before this timestamp.
+   *   **lte** optional
+   *   Return values where the created field is before or equal to this timestamp.
+   *
+   * [customer]: Only return charges for the customer specified by this
+   * customer ID.
+   *
+   * [endingBefore]: A cursor for use in pagination. ending_before is an object
+   * ID that defines your place in the list. For instance, if you make a list
+   * request and receive 100 objects, starting with obj_bar, your subsequent
+   * call can include ending_before=obj_bar in order to fetch the previous page
+   * of the list.
+   *
+   * [limit]: A limit on the number of objects to be returned. Limit can range
+   * between 1 and 100 items.
+   *
+   * [startingAfter] A cursor for use in pagination. starting_after is an object
+   * ID that defines your place in the list. For instance, if you make a list
+   * request and receive 100 objects, ending with obj_foo, your subsequent call
+   * can include starting_after=obj_foo in order to fetch the next page
+   * of the list.
+   */
+  static Future<ChargeCollection> list({String created, String customer, int limit, String startingAfter, String endingBefore}) {
+    Map data = {};
+    if (created != null) data["created"] = created;
+    if (customer != null) data["customer"] = customer;
+    if (limit != null) data["limit"] = limit;
+    if (startingAfter != null) data["starting_after"] = startingAfter;
+    if (endingBefore != null) data["ending_before"] = endingBefore;
+    if (data == {}) data = null;
+    return StripeService.list([Charge._path], data: data)
+        .then((Map json) => new ChargeCollection.fromMap(json));
+  }
+
 }
 
 
