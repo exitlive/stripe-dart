01dbcae2e65c7f93ebf2e47d1c90f208fa5d488b
Martin Flucka <martin.flucka@gmail.com>
2014-05-27 13:52:31 +0200

    Sorting the imports

--- lib/stripe.dart
+++ lib/stripe.dart
@@ -1,24 +1,22 @@
 library stripe;
 
 import 'dart:async';
-import 'dart:io';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:mirrors';
-
 
 import 'package:logging/logging.dart';
 
 part 'src/exceptions.dart';
-
 part 'src/service.dart';
-
 part 'src/annotations.dart';
 
-
+// generic abstract classes
+part 'src/resources/api_resource.dart';
 part 'src/resources/resource.dart';
 part 'src/resources/resource_collection.dart';
-part 'src/resources/api_resource.dart';
 
+// resource classes
 part 'src/resources/account.dart';
 part 'src/resources/application_fee.dart';
 part 'src/resources/balance.dart';
@@ -27,13 +25,13 @@ part 'src/resources/charge.dart';
 part 'src/resources/coupon.dart';
 part 'src/resources/customer.dart';
 part 'src/resources/discount.dart';
+part 'src/resources/dispute.dart';
 part 'src/resources/event.dart';
 part 'src/resources/invoice.dart';
 part 'src/resources/invoice_item.dart';
 part 'src/resources/plan.dart';
-part 'src/resources/dispute.dart';
-part 'src/resources/refund.dart';
 part 'src/resources/recipient.dart';
+part 'src/resources/refund.dart';
 part 'src/resources/subscription.dart';
 part 'src/resources/token.dart';
 part 'src/resources/transfer.dart';
