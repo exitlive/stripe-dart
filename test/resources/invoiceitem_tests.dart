7f697b9453d3022f1f056068df4dd2859da2ff9b
Martin Flucka <martin.flucka@gmail.com>
2015-04-14 17:02:35 +0200

    test: use async await for invoice item tests

--- test/resources/invoiceitem_tests.dart
+++ test/resources/invoiceitem_tests.dart
@@ -1,6 +1,5 @@
 library invoiceitem_tests;
 
-import 'dart:async';
 import 'dart:convert';
 
 import 'package:unittest/unittest.dart';
@@ -63,10 +62,7 @@ main(List<String> args) {
       return utils.tearDown();
     });
 
-    test('Create Invoice item minimal', () {
-
-      // Customer fields
-      Customer testCustomer;
+    test('Create Invoice item minimal', () async {
 
       // Card fields
       String testCardNumber = '4242424242424242';
@@ -77,38 +73,26 @@ main(List<String> args) {
       int testChargeAmount = 100;
       String testChargeCurrency = 'usd';
 
-      new CustomerCreation().create()
-          .then((Customer customer) {
-            testCustomer = customer;
-            return (new CardCreation()
-                ..number = testCardNumber
-                ..expMonth = testCardExpMonth
-                ..expYear = testCardExpYear
-            ).create(testCustomer.id);
-          })
-          .then((Card card) {
-            return (new InvoiceitemCreation()
-                ..customer = testCustomer.id
-                ..amount = testChargeAmount
-                ..currency = testChargeCurrency
-            ).create();
-          })
-          .then((Invoiceitem invoiceitem) {
-            expect(invoiceitem.amount, equals(testChargeAmount));
-            expect(invoiceitem.currency, equals(testChargeCurrency));
-            expect(invoiceitem.customer, equals(testCustomer.id));
-          })
-          .then(expectAsync((_) => true));
+      Customer customer = await new CustomerCreation().create();
+      await (new CardCreation()
+          ..number = testCardNumber
+          ..expMonth = testCardExpMonth
+          ..expYear = testCardExpYear
+      ).create(customer.id);
+      Invoiceitem invoiceitem = await (new InvoiceitemCreation()
+          ..customer = customer.id
+          ..amount = testChargeAmount
+          ..currency = testChargeCurrency
+      ).create();
+      expect(invoiceitem.amount, equals(testChargeAmount));
+      expect(invoiceitem.currency, equals(testChargeCurrency));
+      expect(invoiceitem.customer, equals(customer.id));
 
     });
 
-    test('Create Invoice item full', () {
-
-      // Customer fields
-      Customer testCustomer;
+    test('Create Invoice item full', () async {
 
       // Card fields
-      Card testCard;
       String testCardNumber = '4242424242424242';
       int testCardExpMonth = 12;
       int testCardExpYear = 2016;
@@ -119,22 +103,16 @@ main(List<String> args) {
           ..expYear = testCardExpYear;
 
       // Charge fields
-      int testChargeAmount = 100;
       String testChargeCurrency = 'usd';
 
       // Plan fields
-      Plan testPlan;
       String testPlanId = 'test plan id';
       int testPlanAmount = 200;
       String testPlanCurrency = 'usd';
       String testPlanInterval = 'month';
       String testPlanName = 'test plan name';
 
-      // Subscription fields
-      Subscription testSubscription;
-
       // Invoiceitem fields
-      Invoiceitem testInvoiceitem;
       String testInvoiceitemCurrency = 'usd';
       int testInvoiceitemAmount1 = 200;
       String testInvoiceitemDescription1 = 'test description1';
@@ -144,84 +122,58 @@ main(List<String> args) {
       String testInvoiceitemDescription2 = 'test description2';
       Map testInvoiceitemMetadata2 = {'foo': 'bar2'};
 
-      (new PlanCreation()
+      Plan plan = await (new PlanCreation()
           ..id = testPlanId
           ..amount = testPlanAmount
           ..currency = testPlanCurrency
           ..interval = testPlanInterval
           ..name = testPlanName
-      ).create()
-          .then((Plan plan) {
-            testPlan = plan;
-            return new CustomerCreation().create();
-          })
-          .then((Customer customer) {
-            testCustomer = customer;
-            return testCardCreation.create(testCustomer.id);
-          })
-          .then((Card card) {
-            testCard = card;
-            return (
-                new SubscriptionCreation()
-                    ..plan = testPlan.id
-            ).create(testCustomer.id);
-          })
-          .then((Subscription subscription) {
-            testSubscription = subscription;
-            return (new InvoiceitemCreation()
-                ..customer = testCustomer.id
-                ..amount = testInvoiceitemAmount1
-                ..currency = testInvoiceitemCurrency
-                ..subscription = subscription.id
-                ..description = testInvoiceitemDescription1
-                ..metadata = testInvoiceitemMetadata1
-            ).create();
-          })
-          .then((Invoiceitem invoiceitem) {
-            expect(invoiceitem.amount, equals(testInvoiceitemAmount1));
-            expect(invoiceitem.currency, equals(testInvoiceitemCurrency));
-            expect(invoiceitem.customer, equals(testCustomer.id));
-            expect(invoiceitem.subscription, equals(testSubscription.id));
-            expect(invoiceitem.description, equals(testInvoiceitemDescription1));
-            expect(invoiceitem.metadata, equals(testInvoiceitemMetadata1));
-            // seems that expanding currently does not work according to the API specification
-            // testing retrieve
-            return Invoiceitem.retrieve(invoiceitem.id);
-          })
-          .then((Invoiceitem invoiceitem) {
-            expect(invoiceitem.amount, equals(testInvoiceitemAmount1));
-            expect(invoiceitem.currency, equals(testChargeCurrency));
-            expect(invoiceitem.customer, equals(testCustomer.id));
-            expect(invoiceitem.subscription, equals(testSubscription.id));
-            expect(invoiceitem.description, equals(testInvoiceitemDescription1));
-            expect(invoiceitem.metadata, equals(testInvoiceitemMetadata1));
-            // testing InvoiceitemUpdate
-            return (new InvoiceitemUpdate()
-                ..amount = testInvoiceitemAmount2
-                ..description = testInvoiceitemDescription2
-                ..metadata = testInvoiceitemMetadata2
-            ).update(invoiceitem.id);
-          })
-          .then((Invoiceitem invoiceitem) {
-            testInvoiceitem = invoiceitem;
-            expect(invoiceitem.amount, equals(testInvoiceitemAmount2));
-            expect(invoiceitem.description, equals(testInvoiceitemDescription2));
-            expect(invoiceitem.metadata, equals(testInvoiceitemMetadata2));
-            // testing delete
-            return Invoiceitem.delete(invoiceitem.id);
-          })
-          .then((Map response) {
-            expect(response['id'], equals(testInvoiceitem.id));
-            expect(response['deleted'], isTrue);
-          })
-          .then(expectAsync((_) => true));
+      ).create();
+      Customer customer = await new CustomerCreation().create();
+      await testCardCreation.create(customer.id);
+      Subscription subscription = await (new SubscriptionCreation()
+          ..plan = plan.id
+      ).create(customer.id);
+      Invoiceitem invoiceitem = await (new InvoiceitemCreation()
+          ..customer = customer.id
+          ..amount = testInvoiceitemAmount1
+          ..currency = testInvoiceitemCurrency
+          ..subscription = subscription.id
+          ..description = testInvoiceitemDescription1
+          ..metadata = testInvoiceitemMetadata1
+      ).create();
+      expect(invoiceitem.amount, equals(testInvoiceitemAmount1));
+      expect(invoiceitem.currency, equals(testInvoiceitemCurrency));
+      expect(invoiceitem.customer, equals(customer.id));
+      expect(invoiceitem.subscription, equals(subscription.id));
+      expect(invoiceitem.description, equals(testInvoiceitemDescription1));
+      expect(invoiceitem.metadata, equals(testInvoiceitemMetadata1));
+      // seems that expanding currently does not work according to the API specification
+      // testing retrieve
+      invoiceitem = await Invoiceitem.retrieve(invoiceitem.id);
+      expect(invoiceitem.amount, equals(testInvoiceitemAmount1));
+      expect(invoiceitem.currency, equals(testChargeCurrency));
+      expect(invoiceitem.customer, equals(customer.id));
+      expect(invoiceitem.subscription, equals(subscription.id));
+      expect(invoiceitem.description, equals(testInvoiceitemDescription1));
+      expect(invoiceitem.metadata, equals(testInvoiceitemMetadata1));
+      // testing InvoiceitemUpdate
+      invoiceitem = await (new InvoiceitemUpdate()
+          ..amount = testInvoiceitemAmount2
+          ..description = testInvoiceitemDescription2
+          ..metadata = testInvoiceitemMetadata2
+      ).update(invoiceitem.id);
+      expect(invoiceitem.amount, equals(testInvoiceitemAmount2));
+      expect(invoiceitem.description, equals(testInvoiceitemDescription2));
+      expect(invoiceitem.metadata, equals(testInvoiceitemMetadata2));
+      // testing delete
+      Map response = await Invoiceitem.delete(invoiceitem.id);
+      expect(response['id'], equals(invoiceitem.id));
+      expect(response['deleted'], isTrue);
 
     });
 
-    test('List parameters Invoiceitem', () {
-
-      // Customer fields
-      Customer testCustomer;
+    test('List parameters Invoiceitem', () async {
 
       // Card fields
       String testCardNumber = '4242424242424242';
@@ -232,44 +184,31 @@ main(List<String> args) {
       int testChargeAmount = 100;
       String testChargeCurrency = 'usd';
 
-      List<Future> queue = [];
+      Customer customer = await new CustomerCreation().create();
+
+      await (new CardCreation()
+          ..number = testCardNumber
+          ..expMonth = testCardExpMonth
+          ..expYear = testCardExpYear
+      ).create(customer.id);
+
       for (var i = 0; i < 20; i++) {
-        queue.add(
-          new CustomerCreation().create()
-              .then((Customer customer) {
-                testCustomer = customer;
-                return (new CardCreation()
-                    ..number = testCardNumber
-                    ..expMonth = testCardExpMonth
-                    ..expYear = testCardExpYear
-                ).create(testCustomer.id);
-          })
-          .then((Card card) {
-            return (new InvoiceitemCreation()
-                ..customer = testCustomer.id
-                ..amount = testChargeAmount
-                ..currency = testChargeCurrency
-            ).create();
-          })
-        );
+        await (new InvoiceitemCreation()
+            ..customer = customer.id
+            ..amount = testChargeAmount
+            ..currency = testChargeCurrency
+        ).create();
       }
-      Future.wait(queue)
-          .then((_) => Invoiceitem.list(limit: 10))
-          .then((InvoiceitemCollection invoiceitems) {
-            expect(invoiceitems.data.length, equals(10));
-            expect(invoiceitems.hasMore, equals(true));
-            return Invoiceitem.list(limit: 10, startingAfter: invoiceitems.data.last.id);
-          })
-          .then((InvoiceitemCollection invoiceitems) {
-            expect(invoiceitems.data.length, equals(10));
-            expect(invoiceitems.hasMore, equals(false));
-            return Invoiceitem.list(limit: 10, endingBefore: invoiceitems.data.first.id);
-          })
-          .then((InvoiceitemCollection invoiceitems) {
-            expect(invoiceitems.data.length, equals(10));
-            expect(invoiceitems.hasMore, equals(false));
-          })
-          .then(expectAsync((_) => true));
+
+      InvoiceitemCollection invoiceitems = await Invoiceitem.list(limit: 10);
+      expect(invoiceitems.data.length, equals(10));
+      expect(invoiceitems.hasMore, equals(true));
+      invoiceitems = await Invoiceitem.list(limit: 10, startingAfter: invoiceitems.data.last.id);
+      expect(invoiceitems.data.length, equals(10));
+      expect(invoiceitems.hasMore, equals(false));
+      invoiceitems = await Invoiceitem.list(limit: 10, endingBefore: invoiceitems.data.first.id);
+      expect(invoiceitems.data.length, equals(10));
+      expect(invoiceitems.hasMore, equals(false));
 
     });
 
