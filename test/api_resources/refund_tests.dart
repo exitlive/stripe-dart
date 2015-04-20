fb63602f229e8ee91176c08d48cce9a50c650157
Martin Flucka <martin.flucka@gmail.com>
2015-04-20 19:44:25 +0200

    chore update refund and tests

--- test/api_resources/refund_tests.dart
+++ test/api_resources/refund_tests.dart
@@ -40,4 +40,113 @@ main(List<String> args) {
       expect(refund.amount, map['amount']);
     });
   });
+
+  group('Refund online', () {
+    tearDown(() {
+      return utils.tearDown();
+    });
+
+    test('RefundCreation minimal', () async {
+
+      // Card fields
+      var cardNumber = '4242424242424242',
+          cardExpMonth = 12,
+          cardExpYear = 2016,
+
+          // Charge fields
+          chargeAmount = 100,
+          chargeCurrency = 'usd';
+
+      var customer = await new CustomerCreation().create();
+      await (new CardCreation()
+        ..number = cardNumber
+        ..expMonth = cardExpMonth
+        ..expYear = cardExpYear).create(customer.id);
+      var charge = await (new ChargeCreation()
+        ..amount = chargeAmount
+        ..currency = chargeCurrency
+        ..customer = customer.id).create();
+      var refund = await new RefundCreation().create(charge.id);
+      expect(refund.amount, chargeAmount);
+      expect(refund.currency, chargeCurrency);
+      expect(refund.charge, charge.id);
+    });
+
+    test('ChargeCreation full', () async {
+
+      // Card fields
+      var cardNumber = '4242424242424242',
+          cardExpMonth = 12,
+          cardExpYear = 2016,
+
+          // Charge fields
+          chargeAmount = 100,
+          chargeCurrency = 'usd',
+
+          // Refund fields
+          refundAmount = 50,
+          refundReason = 'fraudulent',
+          refundMetadata1 = {'foo': 'bar1'},
+          refundMetadata2 = {'foo': 'bar2'};
+
+      var customer = await new CustomerCreation().create();
+      await (new CardCreation()
+        ..number = cardNumber
+        ..expMonth = cardExpMonth
+        ..expYear = cardExpYear).create(customer.id);
+      var charge = await (new ChargeCreation()
+        ..amount = chargeAmount
+        ..currency = chargeCurrency
+        ..customer = customer.id).create();
+      var refund = await (new RefundCreation()
+        ..amount = refundAmount
+        ..reason = refundReason
+        ..metadata = refundMetadata1).create(charge.id);
+      expect(refund.amount, refundAmount);
+      expect(refund.reason, refundReason);
+      expect(refund.metadata, refundMetadata1);
+      // testing the expand functionality of retrieve
+      refund = await Refund.retrieve(charge.id, refund.id, data: {'expand': ['balance_transaction']});
+      expect(refund.balanceTransactionExpand.amount, -refundAmount);
+      expect(refund.balanceTransactionExpand.currency, chargeCurrency);
+      expect(refund.balanceTransactionExpand.type, 'refund');
+      expect(refund.balanceTransactionExpand.source, charge.id);
+      // testing the ChargeUpdate
+      refund = await (new RefundUpdate()..metadata = refundMetadata2).update(charge.id, refund.id);
+      expect(refund.metadata, refundMetadata2);
+    });
+
+    test('List parameters refund', () async {
+      var cardNumber = '4242424242424242',
+          cardExpMonth = 12,
+          cardExpYear = 2016,
+
+          // Charge fields
+          chargeAmount = 100,
+          chargeCurrency = 'usd';
+
+      var customer = await new CustomerCreation().create();
+      await (new CardCreation()
+        ..number = cardNumber
+        ..expMonth = cardExpMonth
+        ..expYear = cardExpYear).create(customer.id);
+      var charge = await (new ChargeCreation()
+        ..amount = chargeAmount
+        ..currency = chargeCurrency
+        ..customer = customer.id).create();
+
+      for (var i = 0; i < 20; i++) {
+        await (new RefundCreation()..amount = 5).create(charge.id);
+      }
+      var charges = await Refund.list(charge.id, limit: 10);
+      expect(charges.data.length, 10);
+      expect(charges.hasMore, isTrue);
+      charges = await Refund.list(charge.id, limit: 10, startingAfter: charges.data.last.id);
+      expect(charges.data.length, 10);
+      expect(charges.hasMore, isFalse);
+      charges = await Refund.list(charge.id, limit: 10, endingBefore: charges.data.first.id);
+      expect(charges.data.length, 10);
+      expect(charges.hasMore, isFalse);
+    });
+  });
 }
