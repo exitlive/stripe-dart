8e70da3c0bba17900a0cb0528a8b43be3f604a10
Martin Flucka <martin.flucka@gmail.com>
2015-04-23 18:05:19 +0200

    chore: update account and tests

--- test/api_resources/account_tests.dart
+++ test/api_resources/account_tests.dart
@@ -7,24 +7,42 @@ import 'package:unittest/unittest.dart';
 import '../../lib/stripe.dart';
 import '../utils.dart' as utils;
 
-var exampleAccount = '''
+import '../resources/bank_account_tests.dart' as bank_account;
+import '../resources/legal_entity_tests.dart' as legal_entity;
+import '../resources/tos_acceptance_tests.dart' as tos_acceptance;
+import '../resources/transfer_schedule_tests.dart' as transfer_schedule;
+import '../resources/verification_tests.dart' as verification;
+
+
+var example = '''
     {
-      "id": "acct_103yoB41dfVNZFcq",
-      "email": "martin.flucka@gmail.com",
-      "statement_descriptor": null,
-      "display_name": null,
-      "timezone": "Etc/UTC",
-      "details_submitted": false,
-      "charge_enabled": false,
-      "transfer_enabled": false,
+      "id": "acct_102yoB41dfVNZFcq",
+      "object": "account",
+      "charges_enabled": true,
+      "country": "US",
       "currencies_supported": [
         "usd",
-        "aed",
-        "afn"
+        "cad"
       ],
       "default_currency": "usd",
-      "country": "US",
-      "object": "account"
+      "details_submitted": true,
+      "transfers_enabled": true,
+      "display_name": "display_name",
+      "email": "test@test.com",
+      "statement_descriptor": "statement_descriptor",
+      "timezone": "Etc/UTC",
+      "business_name": "business_name",
+      "business_url": "business_url",
+      "metadata": ${utils.metadataExample},
+      "support_phone": "support_phone",
+      "managed": true,
+      "bank_accounts": ${bank_account.collectionExample},
+      "debit_negative_balances": true,
+      "legal_entity": ${legal_entity.example},
+      "product_description": "product_description",
+      "tos_acceptance": ${tos_acceptance.example},
+      "transfer_schedule": ${transfer_schedule.example},
+      "verification": ${verification.example}
     }''';
 
 main(List<String> args) {
@@ -32,19 +50,30 @@ main(List<String> args) {
 
   group('Account offline', () {
     test('fromMap() properly popullates all values', () {
-      var map = JSON.decode(exampleAccount);
+      var map = JSON.decode(example);
       var account = new Account.fromMap(map);
       expect(account.id, map['id']);
-      expect(account.email, map['email']);
-      expect(account.statementDescriptor, map['statement_descriptor']);
-      expect(account.displayName, map['display_name']);
-      expect(account.timezone, map['timezone']);
-      expect(account.detailsSubmitted, map['details_submitted']);
-      expect(account.chargeEnabled, map['charge_enabled']);
-      expect(account.transferEnabled, map['transfer_enabled']);
+      expect(account.country, map['country']);
       expect(account.currenciesSupported, map['currencies_supported']);
       expect(account.defaultCurrency, map['default_currency']);
-      expect(account.country, map['country']);
+      expect(account.detailsSubmitted, map['details_submitted']);
+      expect(account.transfersEnabled, map['transfers_enabled']);
+      expect(account.displayName, map['display_name']);
+      expect(account.email, map['email']);
+      expect(account.statementDescriptor, map['statement_descriptor']);
+      expect(account.timezone, map['timezone']);
+      expect(account.businessName, map['business_name']);
+      expect(account.businessUrl, map['business_url']);
+      expect(account.metadata, map['metadata']);
+      expect(account.supportPhone, map['support_phone']);
+      expect(account.managed, map['managed']);
+      expect(account.bankAccounts.toMap(), new BankAccount.fromMap(map['bank_accounts']).toMap());
+      expect(account.debitNegativeBalances, map['debit_negative_balances']);
+      expect(account.legalEntity.toMap(), new LegalEntity.fromMap(map['legal_entity']).toMap());
+      expect(account.productDescription, map['product_description']);
+      expect(account.tosAcceptance.toMap(), new TosAcceptance.fromMap(map['tos_acceptance']).toMap());
+      expect(account.transferSchedule.toMap(), new TransferSchedule.fromMap(map['transfer_schedule']).toMap());
+      expect(account.verification.toMap(), new Verification.fromMap(map['verification']).toMap());
     });
   });
 
