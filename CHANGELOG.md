d62e4cf0edc66f4068c63eba94c64bca4f250f7a
Martin Flucka <martin.flucka@gmail.com>
2016-08-08 16:37:32 +0200

    Add CHANGELOG.md (#34)
    * style: apply code format
    
    * feat: add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## 1.2.1 (2016-08-08)
+
+Features:
+
+  - add CHANGELOG.md (closes [#32](https://github.com/exitlive/stripe-dart/issues/32))
+  - add default source to customer ([#33](https://github.com/exitlive/stripe-dart/pull/33), [@rbellens](https://github.com/rbellens))
+
+Bug fixes:
+
+  - fix card creation with token ([#33](https://github.com/exitlive/stripe-dart/pull/33), [@rbellens](https://github.com/rbellens))
--- lib/src/api_resources/card.dart
+++ lib/src/api_resources/card.dart
@@ -135,8 +135,7 @@ class CardCreationWithToken extends ResourceRequest implements SourceCreation {
   }
 
   Future<Card> create(String customerId) async {
-    var dataMap = await StripeService.create([Customer._path, customerId, Card._path],
-        {"source": _getMap()});
+    var dataMap = await StripeService.create([Customer._path, customerId, Card._path], {'source': _getMap()});
     return new Card.fromMap(dataMap);
   }
 }
